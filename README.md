d706371e3143acec55800c6fadace71cc4c5e1a6
enzoranelli <enzo.ranelli@gmail.com>
2026-03-27 16:35:10 -0300

    Agrego readme. Modelos de categoria y producto

---
+++ README.md
@@ -0,0 +1,4 @@
+# Proyecto ecommerce
+
+## Modelo de la base de datos
+![alt text](ecommerce.drawio.png)
--- src/main/java/com/enzoranelli/ecommerce/model/Producto.java
+++ src/main/java/com/enzoranelli/ecommerce/model/Producto.java
@@ -1,9 +1,8 @@
 package com.enzoranelli.ecommerce.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
 
 @Entity
 public class Producto {
@@ -12,9 +11,59 @@ public class Producto {
     private Long id;
     private String nombre;
     private String descripcion;
-    private double precio;
+    @Column(precision=10, scale = 2)
+    private BigDecimal precio;
     private int stock;
     private String imagen;
-    private String categoria;
+    @ManyToOne
+    @JoinColumn(name = "categoria", nullable = false)
+    private Categoria categoria;
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public BigDecimal getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(BigDecimal precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
 }
